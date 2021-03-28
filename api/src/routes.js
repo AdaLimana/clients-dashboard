6ca7c4b894cbac977644bc441e46d20275819bea
Adair J. Limana <ada_limana@yahoo.com.br>
2021-03-28 20:38:29 -0300

    feat(api): configuradas as routes para o controller client.controller

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,12 +1,14 @@
+const clientController = require('./controllers/client.controller');
+
 module.exports.load = (app) => {
   /** Get all clients */
-  app.get("/", (req, res, next) => {});  
+  app.get("/", clientController.clientList);  
 
   /** Get clients by name */
-  app.get("/name/:name", (req, res, next) => {});
+  app.get("/name/:name", clientController.clientByName);
   
   /** Get client totals */
-  app.get("/:client_id/totals", (req, res, next) => {});  
+  app.get("/:client_id/totals", clientController.clientTotals);  
 
   /** Get all enterprises */
   app.get("/enterprise", (req, res, next) => {});
@@ -15,14 +17,14 @@ module.exports.load = (app) => {
   app.get("/enterprise/name/:name", (req, res, next) => {});
 
   /** Get all enterprises by client */
-  app.get("/:client_id/enterprise", (req, res, next) => {});
+  app.get("/:client_id/enterprise", clientController.clientEnterprises);
 
   /** Get enterprises by client and name */
-  app.get("/:client_id/enterprise/name/:name", (req, res, next) => {});
+  app.get("/:client_id/enterprise/name/:name", clientController.clientEnterprisesByName);
 
   /** Get general totals */
-  app.get("/totals", (req, res, next) => {});
+  app.get("/totals", clientController.totals);
 
   /** Get a client by _id */
-  app.get("/:_id", (req, res, next) => {});
+  app.get("/:_id", clientController.clientById);
 };
